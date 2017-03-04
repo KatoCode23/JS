a9c0aa26943c2478614a56555ce7bf255adf9978
Sina <sin.jaz@gmail.com>
2017-03-04 15:57:49 -0500

    Reverse array, ruining kalens life

--- reverse.js
+++ reverse.js
@@ -1,14 +1,6 @@
 function reverse(originalArray) {
-  var reverseArray = [];
-  var originalArrayLength = originalArray.length;
-
-   var randomNumber = Math.floor(Math.random() * originalArray.length)
-   var randomElement = originalArray[randomNumber];
-    shuffledArray.push(randomElement);
-    originalArray.splice(randomNumber,1);
-
-  return 'Here is your reverse Array: ' + reverseArray;
+  var reversedArray = [];
+  return 'Here is your reverse Array: ' + reversedArray;
 }
-
 
 console.log(reverse(['Kalen','Sina','Tara','Eleen','Kara','Dino','John']));
