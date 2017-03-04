de49b70bf073c9f9b4a9013860feacaaae99351a
KatoCode23 <Kalen23Imani@gmail.com>
2017-03-04 15:56:49 -0500

    reverse syntax

--- reverse.js
+++ reverse.js
@@ -1,1 +1,14 @@
-Hello!!!
+function reverse(originalArray) {
+  var reverseArray = [];
+  var originalArrayLength = originalArray.length;
+
+   var randomNumber = Math.floor(Math.random() * originalArray.length)
+   var randomElement = originalArray[randomNumber];
+    shuffledArray.push(randomElement);
+    originalArray.splice(randomNumber,1);
+
+  return 'Here is your reverse Array: ' + reverseArray;
+}
+
+
+console.log(reverse(['Kalen','Sina','Tara','Eleen','Kara','Dino','John']));
