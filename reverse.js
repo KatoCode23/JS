fdb7e4ae647b9cc74a875709924d1b7b47959211
KatoCode23 <Kalen23Imani@gmail.com>
2017-03-05 09:07:38 -0500

    console first number

--- reverse.js
+++ reverse.js
@@ -1,5 +1,10 @@
 function reverse(originalArray) {
   var reversedArray = [];
+  var firstNumber = originalArray[0];
+  console.log(firstNumber);
+  for (var i = 0; i < originalArray.length; i++){
+
+  }
   return 'Here is your reverse Array: ' + reversedArray;
 }
 
