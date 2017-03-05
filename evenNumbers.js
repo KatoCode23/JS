a0a983703d7d675ab28485800bc8926c44af5373
KatoCode23 <Kalen23Imani@gmail.com>
2017-03-05 14:26:33 -0500

    even numbers works

---
+++ evenNumbers.js
@@ -0,0 +1,13 @@
+function evenNumbers(originalArray) {
+  var evenNumbersArray = [];
+
+  for (var i = 0; i < originalArray.length; i++){
+    if(originalArray[i] % 2 === 0 ){
+      evenNumbersArray.push(originalArray[i]);
+    }
+    //console.log(originalArray[i]);
+  }
+  return 'Here is your even numbers Array: ' + evenNumbersArray;
+}
+
+console.log(evenNumbers([1,2,3,4,5,6,7,8,9,10]));
