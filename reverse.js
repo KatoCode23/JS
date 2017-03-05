4f3f6ae461d967abbe996aa95991dadb9f65133d
KatoCode23 <Kalen23Imani@gmail.com>
2017-03-05 13:03:59 -0500

    reverse array works

--- reverse.js
+++ reverse.js
@@ -1,11 +1,8 @@
 function reverse(originalArray) {
   var reversedArray = [];
-  var firstNumber = originalArray[0];
-  console.log(firstNumber);
-  for (var i = 0; i < originalArray.length; i++){
-
-  }
+  var flippedArray = originalArray.reverse();
+  reversedArray = flippedArray;
+  //console.log(copyOfArray);
   return 'Here is your reverse Array: ' + reversedArray;
 }
-
 console.log(reverse(['Kalen','Sina','Tara','Eleen','Kara','Dino','John']));
