79409eb4a230988ff6fc02d8451d63a00b341fa3
KatoCode23 <Kalen23Imani@gmail.com>
2017-03-05 20:53:57 -0500

    reverse bitch

--- reverse.js
+++ reverse.js
@@ -1,8 +1,11 @@
 function reverse(originalArray) {
   var reversedArray = [];
-  var flippedArray = originalArray.reverse();
-  reversedArray = flippedArray;
-  //console.log(copyOfArray);
+  var originalArrayLength = originalArray.length;
+  for(var i = 0; i < originalArrayLength; i++){
+    var poppedNumber = originalArray.pop(originalArray[i]);
+    reversedArray.push(poppedNumber);
+  }
+
   return 'Here is your reverse Array: ' + reversedArray;
 }
 console.log(reverse(['Kalen','Sina','Tara','Eleen','Kara','Dino','John']));
