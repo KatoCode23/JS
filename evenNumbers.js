128a91fe596bb11e35c1891b77e410a821432208
KatoCode23 <Kalen23Imani@gmail.com>
2017-03-15 09:33:25 -0400

    any update I missed

--- evenNumbers.js
+++ evenNumbers.js
@@ -10,4 +10,4 @@ function evenNumbers(originalArray) {
   return 'Here is your even numbers Array: ' + evenNumbersArray;
 }
 
-console.log(evenNumbers([1,2,3,4,5,6,7,8,9,10]));
+console.log(evenNumbers([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]));
