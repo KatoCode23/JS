83a014cfc3cfdacd9f33cc78ab3c9d4f8d6efb4a
KatoCode23 <Kalen23Imani@gmail.com>
2017-03-15 10:17:05 -0400

    playing with indexOf a bit

--- evenNumbers.js
+++ evenNumbers.js
@@ -11,3 +11,5 @@ function evenNumbers(originalArray) {
 }
 
 console.log(evenNumbers([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]));
+var position = [3,6,8,12,18,14,11];
+console.log(position.indexOf(12));
