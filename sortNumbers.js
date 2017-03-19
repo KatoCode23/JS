766d0eea981fcad9cd3066942d4b2fc816253059
KatoCode23 <Kalen23Imani@gmail.com>
2017-03-19 17:06:20 -0400

    sort numbers no error message

--- sortNumbers.js
+++ sortNumbers.js
@@ -2,13 +2,13 @@ function sortNumbers(originalArray){
 var sortedArray = [];
 var originalArrayLength = originalArray.length;
 for (var i = 0; i < originalArrayLength; i++){
-  if(typeOf(originalArray[i]) === 'number'){
+  if(typeof(originalArray[i]) === 'number'){
     sortedArray.push(originalArray[i]);
-    sortedArray.sort(originalArray[i]);
+    sortedArray.sort(function (a,b){return a - b});
   }
 }
 
 return 'Here is my sorted array: ' + sortedArray;
 }
 
-console.log(sortNumbers([7,14,'Sina', null, false, 78, 'boo',10,6]));
+console.log(sortNumbers([7,14,'Sina', null, false, 78, 'boo',10,6,1,39,23,8]));
