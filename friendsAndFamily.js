1c049e905e9c916c22b47dde2824d729126e5f6b
KatoCode23 <Kalen23Imani@gmail.com>
2017-03-21 21:20:00 -0400

    iterate through array

--- friendsAndFamily.js
+++ friendsAndFamily.js
@@ -1,20 +1,17 @@
 function friendsAndFamily (originalArray, acceptedFriends) {
-var friendsAndFamilyArray = [];
-acceptedFriends = ['Kalen', 'Eleen', 'Sina', 'Tara'];
-acceptedFriendsLength = acceptedFriends.length;
-for(var i = 0; i < acceptedFriendsLength; i++ ){
-  var accepted = acceptedFriends[i];
-  console.log(accepted);
+     var friendsAndFamilyArray = [];
+     acceptedFriends = ['Kalen', 'Eleen', 'Sina', 'Tara'];
+     acceptedFriendsLength = acceptedFriends.length;
+     var originalArrayLength = originalArray.length;
+for(var i = 0; i < acceptedFriendsLength; i++){
+     accepted = acceptedFriends[i];
+     if(originalArray.indexOf(accepted) > -1) {
+       friendsAndFamilyArray.push(accepted);
+     }
 }
-var originalArrayLength = originalArray.length;
-for(var i = 0; i < originalArrayLength; i++){
-  if(originalArray[i] === accepted){
-    friendsAndFamilyArray.push(accepted);
-  }
-}
-
-
 return 'Here is my friends and family array: ' + friendsAndFamilyArray;
 }
 
 console.log(friendsAndFamily([4,'James','Eric','Kalen', 'Eleen', 'Sina', 'Tara', 'Adam', 'Peter', 'Mary', 'Moses']));
+console.log(friendsAndFamily(['Cow','Pizza','Kalen', 'Noodles', 'Sina', 'Tara', 'false', null, 17, 'Moses']));
+console.log(friendsAndFamily(['boo','Protein','Kalen', 'Eleen', 'Sina', 'Tara', 'Rocko', 'Beata', null, 17, 'Moses']));
